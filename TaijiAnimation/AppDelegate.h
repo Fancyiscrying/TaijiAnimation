ab2020115b01e1ff52348d7d91ae7c7d6edd9a68
Fancyiscrying <Fancy@fancydeair.mshome.net>
2016-02-29 17:19:34 +0800

    Initial Commit

---
+++ TaijiAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaijiAnimation
+//
+//  Created by Fancy on 16/2/29.
+//  Copyright © 2016年 Fancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaijiAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaijiAnimation
+//
+//  Created by Fancy on 16/2/29.
+//  Copyright © 2016年 Fancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
